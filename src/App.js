0f6040d4eacd1c7be6c8ce6874ad4568f75d3293
anbehindY <yanminthwin@gmail.com>
2023-04-26 23:23:37 +0630

    Add functions to display X when each square is clicked

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React from "react";
+import { useState } from "react";
+
+function Square ({value, handler}) {
+
+  return <button className="square" onClick={handler}>{value}</button>
+}
+
+export default function Board () {
+  const [squaresValue, setSquare] = useState(Array(9).fill(null));
+  const clickHandler = (i) => {
+    // const newSquares = squaresValue.slice();
+    squaresValue[i] = 'X';
+    setSquare(squaresValue);
+  }
+
+  return (
+    <div>
+      <div className="board-row">
+        <Square value={squaresValue[0]} handler={() => clickHandler(0)}/>
+        <Square value={squaresValue[1]} handler={() => clickHandler(1)}/>
+        <Square value={squaresValue[2]} handler={() => clickHandler(2)}/>
+      </div>
+      <div className="board-row">
+      <Square value={squaresValue[3]} handler={() => clickHandler(3)}/>
+      <Square value={squaresValue[4]} handler={() => clickHandler(4)}/>
+      <Square value={squaresValue[5]} handler={() => clickHandler(5)}/>
+      </div>
+      <div className="board-row">
+      <Square value={squaresValue[6]} handler={() => clickHandler(6)}/>
+      <Square value={squaresValue[7]} handler={() => clickHandler(7)}/>
+      <Square value={squaresValue[8]} handler={() => clickHandler(8)}/>
+      </div>
+    </div>
+  );
 }
